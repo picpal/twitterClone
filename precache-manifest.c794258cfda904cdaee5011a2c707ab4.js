ad79cf7a25b6eb7136c1452758e86f268073c210
picpal <picpal@kakao.com>
2022-07-31 13:13:50 +0900

    Updates

---
+++ precache-manifest.c794258cfda904cdaee5011a2c707ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc9890ca0ff6f56d108a265d7b12f13",
+    "url": "/twitterClone/index.html"
+  },
+  {
+    "revision": "cbf580b5bb1e3af681b8",
+    "url": "/twitterClone/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "f4854c2c1f31eb69c6cc",
+    "url": "/twitterClone/static/js/2.2f58bdbd.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/twitterClone/static/js/2.2f58bdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf580b5bb1e3af681b8",
+    "url": "/twitterClone/static/js/main.63b08a64.chunk.js"
+  },
+  {
+    "revision": "d9d6f6d0795e7351b5e9",
+    "url": "/twitterClone/static/js/runtime-main.7427a13f.js"
+  }
+]);
